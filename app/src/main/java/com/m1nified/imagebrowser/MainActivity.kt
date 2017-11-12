0de944e13ebab02d39e280b60010093018c9bd0f
Minified <gora.pub@gmail.com>
2017-11-12 19:25:46 +0100

    added navigation

--- app/src/main/java/com/m1nified/imagebrowser/MainActivity.kt
+++ app/src/main/java/com/m1nified/imagebrowser/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.TextView
 import com.m1nified.helloworld.DownloadImageTask
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Integer.max
+import java.lang.Integer.min
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     init{
         imageItems.add(ImageItem("Obrazek 1", "https://www.w3schools.com/howto/img_fjords.jpg"))
-        imageItems.add(ImageItem("Obrazek 2", "https://www.w3schools.com/howto/img_fjords.jpg"))
-        imageItems.add(ImageItem("Obrazek 3", "https://www.w3schools.com/howto/img_fjords.jpg"))
+        imageItems.add(ImageItem("Obrazek 2", "https://www.w3schools.com/w3css/img_lights.jpg"))
+        imageItems.add(ImageItem("Obrazek 3", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsXUtxNPqBET8CdLgZ-ByWd6pa9AQioyOl-Drf2G7dhaC65irp6Q"))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +25,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         this.updateView()
+
+        imageButtonNext.setOnClickListener{
+            this.showNext()
+        }
+
+        imageButtonPrev.setOnClickListener{
+            this.showPrev()
+        }
 
     }
 
@@ -32,6 +42,17 @@ class MainActivity : AppCompatActivity() {
             editTextTitle.setText(imageItem.title)
             editTextUrl.setText(imageItem.srcUrl)
             DownloadImageTask(imageView).execute(imageItem.srcUrl)
+            textViewPosition.text = "${this.activeIndex + 1}/${this.imageItems.count()}"
         }
+    }
+
+    fun showNext(){
+        this.activeIndex = min(this.activeIndex + 1, this.imageItems.count() - 1)
+        this.updateView()
+    }
+
+    fun showPrev(){
+        this.activeIndex = max(this.activeIndex-1, 0)
+        this.updateView()
     }
 }
