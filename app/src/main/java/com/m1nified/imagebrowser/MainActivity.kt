875a52eeedb57ea0902fb4d275e074bc532fa7d0
Minified <gora.pub@gmail.com>
2017-11-12 19:17:52 +0100

    updateView function

--- app/src/main/java/com/m1nified/imagebrowser/MainActivity.kt
+++ app/src/main/java/com/m1nified/imagebrowser/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.m1nified.imagebrowser
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.m1nified.helloworld.DownloadImageTask
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var imageItems : MutableList<ImageItem> = mutableListOf<ImageItem>()
+    var activeIndex: Int = 0
 
     init{
         imageItems.add(ImageItem("Obrazek 1", "https://www.w3schools.com/howto/img_fjords.jpg"))
@@ -17,5 +22,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.updateView()
+
+    }
+
+    fun updateView(){
+        if(this.imageItems.count() > 0 && this.activeIndex >= 0 && this.activeIndex <= this.imageItems.count()){
+            val imageItem = this.imageItems[activeIndex]
+            editTextTitle.setText(imageItem.title)
+            editTextUrl.setText(imageItem.srcUrl)
+            DownloadImageTask(imageView).execute(imageItem.srcUrl)
+        }
     }
 }
