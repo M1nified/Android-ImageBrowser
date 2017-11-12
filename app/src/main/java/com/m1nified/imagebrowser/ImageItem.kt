f6c2bf07589ae2bfa7f8b333f3e27ea79c728b10
Minified <gora.pub@gmail.com>
2017-11-12 19:05:58 +0100

    added initial data

--- app/src/main/java/com/m1nified/imagebrowser/ImageItem.kt
+++ app/src/main/java/com/m1nified/imagebrowser/ImageItem.kt
@@ -3,7 +3,4 @@ package com.m1nified.imagebrowser
 /**
  * Created by m1nified on 12.11.2017.
  */
-class ImageItem {
-    var title: String = ""
-    var srcUrl: String = ""
-}
+class ImageItem(var title: String = "", var srcUrl: String = "")
--- app/src/main/java/com/m1nified/imagebrowser/MainActivity.kt
+++ app/src/main/java/com/m1nified/imagebrowser/MainActivity.kt
@@ -5,8 +5,17 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var imageItems : MutableList<ImageItem> = mutableListOf<ImageItem>()
+
+    init{
+        imageItems.add(ImageItem("Obrazek 1", "https://www.w3schools.com/howto/img_fjords.jpg"))
+        imageItems.add(ImageItem("Obrazek 2", "https://www.w3schools.com/howto/img_fjords.jpg"))
+        imageItems.add(ImageItem("Obrazek 3", "https://www.w3schools.com/howto/img_fjords.jpg"))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
