d58d4c3c1128956e060e8de50edd666cf1033186
Minified <gora.pub@gmail.com>
2017-11-14 00:57:28 +0100

    added camera intenet example

--- app/src/main/java/com/m1nified/imagebrowser/ImageItem.kt
+++ app/src/main/java/com/m1nified/imagebrowser/ImageItem.kt
@@ -1,6 +1,8 @@
 package com.m1nified.imagebrowser
 
+import android.graphics.Bitmap
+
 /**
  * Created by m1nified on 12.11.2017.
  */
-class ImageItem(var title: String = "", var srcUrl: String = "")
+class ImageItem(var title: String = "", var srcUrl: String = "", var image: Bitmap? = null)
